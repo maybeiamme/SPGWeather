6d57e95c375bf12b5d81faf1a2bac030835c692a
Shepherd <maybeiamme@gmail.com>
2019-07-25 20:01:09 +0800

    Initial commit with basic project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Shepherd on 25/7/19.
+//  Copyright © 2019 Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
